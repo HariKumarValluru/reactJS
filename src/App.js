13f16e4a272001a7350db842a41648676e96300e
Hari kumar Valluru <anil14353@gmail.com>
2017-11-30 11:49:21 +0530

    Template strings to build fetch url

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import logo from './logo.svg';
 import list from './list.js';
 import { Grid, Row, FormGroup } from 'react-bootstrap';
 
+const DEFAULT_QUERY = 'react';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+// Javascript concatination
+//const url = PATH_BASE + PATH_SEARCH + "?" + PARAM_SEARCH + DEFAULT_QUERY;
+// concatinate using ES6 template strings
+const url = `${PATH_BASE}${PARAM_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
+
 // filter the results by search
 function isSearched(searchTerm){
   return function(item){
